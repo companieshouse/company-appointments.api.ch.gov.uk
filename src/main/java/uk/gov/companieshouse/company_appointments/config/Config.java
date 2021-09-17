1496df7f5db5ca19eda9f6b4ac5ddd85888f49a5
Phil Glover <pglover@companieshouse.gov.uk>
2021-09-17 13:44:43 +0100

    don't need to set Naming strategy in Config class

--- src/main/java/uk/gov/companieshouse/company_appointments/config/Config.java
+++ src/main/java/uk/gov/companieshouse/company_appointments/config/Config.java
@@ -2,7 +2,6 @@ package uk.gov.companieshouse.company_appointments.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import uk.gov.companieshouse.company_appointments.interceptor.AuthenticationInterceptor;
@@ -15,11 +14,6 @@ public class Config implements WebMvcConfigurer {
 
     @Autowired
     private AuthenticationInterceptor authenticationInterceptor;
-
-    @Autowired
-    private void setNamingStrategy(MongoMappingContext mappingContext) {
-        mappingContext.setFieldNamingStrategy(new JsonNamingStrategy());
-    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
