f5a01f7a964e1133ab8142555ce48aa91105ed89
kohareCH <kohare1@companieshouse.gov.uk>
2023-01-25 14:24:22 +0000

    [DSND-1446] Made changes to the Mapper so that date of Birth day would appear in result. also changes made to the mapperTest so that it would have a day value for one of the builders

--- src/main/java/uk/gov/companieshouse/company_appointments/CompanyAppointmentMapper.java
+++ src/main/java/uk/gov/companieshouse/company_appointments/CompanyAppointmentMapper.java
@@ -120,6 +120,7 @@ public class CompanyAppointmentMapper {
     private DateOfBirth mapDateOfBirth(CompanyAppointmentData companyAppointmentData) {
         return Optional.ofNullable(companyAppointmentData.getData().getDateOfBirth())
                 .map(dateOfBirth -> new DateOfBirth(
+                        dateOfBirth.getDayOfMonth(),
                         dateOfBirth.getMonthValue(),
                         dateOfBirth.getYear()))
                 .orElse(null);
