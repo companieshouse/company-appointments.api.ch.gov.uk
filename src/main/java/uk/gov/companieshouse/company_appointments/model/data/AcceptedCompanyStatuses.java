489a75b31a90ca5dc5f352ae41a6fa062b465a6f
kinpang-CH <kpang@companieshouse.gov.uk>
2023-03-23 16:24:53 +0000

    Add new class to validate on company status
    * Create enum class for statuses which reflects what is found in existing Perl code
    * Add parenthesized and failing unit test cases

---
+++ src/main/java/uk/gov/companieshouse/company_appointments/model/data/AcceptedCompanyStatuses.java
@@ -0,0 +1,26 @@
+package uk.gov.companieshouse.company_appointments.model.data;
+
+public enum AcceptedCompanyStatuses {
+    ACTIVE("active"),
+    DISSOLVED("dissolved"),
+    LIQUIDATION("liquidation"),
+    RECEIVERSHIP("receivership"),
+    CONVERTED_CLOSED("converted-closed"),
+    OPEN("open"),
+    CLOSED("closed"),
+    INSOLVENCY_PROCEEDINGS("insolvency-proceedings"),
+    VOLUNTARY_ARRANGEMENT("voluntary-arrangement"),
+    ADMINISTRATION("administration"),
+    REGISTERED("registered"),
+    REMOVED("removed");
+
+    private String validCompanyStatus;
+
+    AcceptedCompanyStatuses(String validCompanyStatus) {
+        this.validCompanyStatus = validCompanyStatus;
+    }
+
+    public String getValidCompanyStatus() {
+        return validCompanyStatus;
+    }
+}
