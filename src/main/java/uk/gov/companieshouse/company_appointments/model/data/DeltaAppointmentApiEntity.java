5eba96343b2410fa7e106043f62dba11ea5033ed
kinpang-CH <kpang@companieshouse.gov.uk>
2023-03-31 11:37:34 +0100

    Remove unneeded fields, setters and constructors from model

--- src/main/java/uk/gov/companieshouse/company_appointments/model/data/DeltaAppointmentApiEntity.java
+++ src/main/java/uk/gov/companieshouse/company_appointments/model/data/DeltaAppointmentApiEntity.java
@@ -10,22 +10,11 @@ public class DeltaAppointmentApiEntity extends DeltaAppointmentApi {
 
     private String companyName;
     private String companyStatus;
-    private String etag;
-    private InstantAPI updatedAt;
-
 
     public DeltaAppointmentApiEntity(final DeltaAppointmentApi api){
         super(api.getId(), api.getEtag(), api.getData(), api.getSensitiveData(), api.getInternalId(),
                 api.getAppointmentId(), api.getOfficerId(), api.getPreviousOfficerId(), api.getCompanyNumber(), api.getUpdatedAt(),
                 api.getUpdatedBy(), api.getCreated(), api.getDeltaAt(), api.getOfficerRoleSortOrder());
-    }
-
-    public DeltaAppointmentApiEntity(final DeltaAppointmentApi api, String companyName, String companyStatus){
-        super(api.getId(), api.getEtag(), api.getData(), api.getSensitiveData(), api.getInternalId(),
-                api.getAppointmentId(), api.getOfficerId(), api.getPreviousOfficerId(), api.getCompanyNumber(), api.getUpdatedAt(),
-                api.getUpdatedBy(), api.getCreated(), api.getDeltaAt(), api.getOfficerRoleSortOrder());
-        this.companyName = companyName;
-        this.companyStatus = companyStatus;
     }
 
     public DeltaAppointmentApiEntity(){}
@@ -38,12 +27,4 @@ public class DeltaAppointmentApiEntity extends DeltaAppointmentApi {
         this.companyStatus = companyStatus;
     }
 
-    @Override
-    public void setEtag(String etag) {
-        this.etag = etag;
-    }
-    @Override
-    public void setUpdatedAt(InstantAPI updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
