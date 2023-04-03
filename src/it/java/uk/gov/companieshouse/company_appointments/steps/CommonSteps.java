4c8457a9de19a8ca23e5bc5916ad3f845c9d3423
crowlandsCH <crowlands@companieshouse.gov.uk>
2023-04-03 16:15:08 +0100

    DSND-1536: WIP integration tests

--- src/it/java/uk/gov/companieshouse/company_appointments/steps/CommonSteps.java
+++ src/it/java/uk/gov/companieshouse/company_appointments/steps/CommonSteps.java
@@ -1,12 +1,10 @@
 package uk.gov.companieshouse.company_appointments.steps;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.cucumber.java.Before;
 import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.java.hu.De;
 import org.apache.commons.io.IOUtils;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,32 +26,25 @@ import uk.gov.companieshouse.api.appointment.ExternalData;
 import uk.gov.companieshouse.api.appointment.FullRecordCompanyOfficerApi;
 import uk.gov.companieshouse.api.appointment.InternalData;
 import uk.gov.companieshouse.api.appointment.SensitiveData;
-import uk.gov.companieshouse.api.metrics.MetricsApi;
-import uk.gov.companieshouse.company_appointments.config.CucumberContext;
 import uk.gov.companieshouse.company_appointments.config.WiremockTestConfig;
 import uk.gov.companieshouse.company_appointments.model.data.DeltaAppointmentApiEntity;
 import uk.gov.companieshouse.company_appointments.repository.CompanyAppointmentFullRecordRepository;
 import uk.gov.companieshouse.company_appointments.util.DeltaDateValidator;
-import uk.gov.companieshouse.company_appointments.util.FileReader;
 import uk.gov.companieshouse.company_appointments.util.JSONMatcher;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.time.OffsetDateTime;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.moreThanOrExactly;
 import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 import static uk.gov.companieshouse.company_appointments.config.AbstractMongoConfig.mongoDBContainer;
 import static uk.gov.companieshouse.company_appointments.config.CucumberContext.CONTEXT;
 
@@ -87,12 +78,6 @@ public class CommonSteps {
         mongoTemplate.insert(Document.parse(
                         IOUtils.resourceToString("/delta-appointment-data.json", StandardCharsets.UTF_8)),
                 "delta_appointments");
-//        mongoTemplate.insert(Document.parse(
-//                        IOUtils.resourceToString("/natural_officer_full_record_PUT.json", StandardCharsets.UTF_8)),
-//                "delta_appointments");
-//        mongoTemplate.insert(Document.parse(
-//                        IOUtils.resourceToString("/corporate_officer_full_record_PUT.json", StandardCharsets.UTF_8)),
-//                "delta_appointments");
 
         WiremockTestConfig.setupWiremock();
 
@@ -110,48 +95,29 @@ public class CommonSteps {
 
     @Given("the delta for payload {string} is the most recent delta for {string} and {string}")
     public void thisDeltaIsNotStale(String dataFile, String companyNumber, String appointmentId) throws IOException {
-//        //essentially want to stub the boolean isDeltaStale method in the CompanyAppointmentFullRecordService here. To return false.
-//        Optional<DeltaAppointmentApiEntity> existingAppointment = Optional.ofNullable(mongoTemplate.findById(HASHED_APPOINTMENT_ID, DeltaAppointmentApiEntity.class));
-////        existingAppointment.get().setDeltaAt();
-//        //can we somehow be clever with the cucumber context and get the appointment delta_at to be set the same as the existing appointment delta_at
-//        // more importantly can we set these delta_at's to be different when we want delta to be stale?
-//        File file = new ClassPathResource("src/it/resources/" + dataFile + ".json").getFile();
-//        DeltaAppointmentApiEntity newAppointment = objectMapper.readValue(file, DeltaAppointmentApiEntity.class);
-//        existingAppointment.get().setDeltaAt("");
-//        newAppointment.setDeltaAt("");
+        Query query = new Query();
+        query.addCriteria(Criteria.where("_id").is("7IjxamNGLlqtIingmTZJJ42Hw9Q"));
+        DeltaAppointmentApiEntity appointment = mongoTemplate.find(query, DeltaAppointmentApiEntity.class)
+                .get(0);
 
-//        Query query = new Query();
-//        query.addCriteria(Criteria.where("_id").is(appointmentId));
-//
-//        List<DeltaAppointmentApiEntity> appointments = mongoTemplate.find(query, DeltaAppointmentApiEntity.class);
-//
-//        for(DeltaAppointmentApiEntity appointment : appointments){
-//            CONTEXT.set("appointmentDeltaAt", appointment.getDeltaAt());
-//        }
+        File file = new ClassPathResource("input/" + dataFile + ".json").getFile();
+        FullRecordCompanyOfficerApi fullRecordCompanyOfficerApi = objectMapper.readValue(file, FullRecordCompanyOfficerApi.class);
+//        OffsetDateTime deltaAt = OffsetDateTime.parse(appointment.getDeltaAt());
+//        fullRecordCompanyOfficerApi.getInternalData().setDeltaAt(deltaAt.plusDays(1L));
 
-            File file = new ClassPathResource("input/" + dataFile + ".json").getFile();
-            FullRecordCompanyOfficerApi fullRecordCompanyOfficerApi = objectMapper.readValue(file, FullRecordCompanyOfficerApi.class);
-
-            fullRecordCompanyOfficerApi.getInternalData().setDeltaAt(OffsetDateTime.parse("2021-04-23T18:25:43.511Z"));
-            String recordData = fullRecordCompanyOfficerApi.toString();
-
-            CONTEXT.set("getRecord", fullRecordCompanyOfficerApi);
-
-            String data = FileReader.readInputFile(dataFile);
-            data.replaceAll("1.411661403E9", ZonedDateTime.now().format(DateTimeFormatter.ISO_INSTANT));
-
-            CONTEXT.set("deltaData", data);
+        CONTEXT.set("getRecord", fullRecordCompanyOfficerApi);
     }
-/*
-problem is when we are mapping it with the object mapper we are adding on a "class FullResourceCompanyOfficerApi"
-at the start of the data in the string where there shouldn't be one.
- Hence 400 bad request when we send it to the controller because it is not in the format of FullRecordCompanyOfficerApi
 
- In the production code as well - we don't need the DeltaDateValidator anymore either as we Aren't needing to test that in that way
- Additionally, Maybe the simplest way of doing what i want to do with this step definition is to have multiple files with different delta_at's?
- Might be easier than messing around with cucumber CONTEXT and objects and an easy way to see if that is the reason for the 400 error.
+//    @Given("the delta for payload {string} is stale")
+//    public void thisDeltaIsStale(String dataFile) throws IOException {
+//        File file = new ClassPathResource("input/" + dataFile + ".json").getFile();
+//        FullRecordCompanyOfficerApi fullRecordCompanyOfficerApi = objectMapper.readValue(file, FullRecordCompanyOfficerApi.class);
+//        OffsetDateTime deltaAt = fullRecordCompanyOfficerApi.getInternalData().getDeltaAt();
+//        fullRecordCompanyOfficerApi.getInternalData().setDeltaAt(deltaAt.plusDays(1L));
+//
+//        CONTEXT.set("getRecord", fullRecordCompanyOfficerApi);
+//    }
 
- */
 
     @Given("the company appointments api is running")
     public void theApplicationRunning() {
@@ -162,9 +128,7 @@ at the start of the data in the string where there shouldn't be one.
     @When("I send a PUT request with payload {string}")
     public void sendPutRequest(String payloadFile) throws IOException {
         FullRecordCompanyOfficerApi fullRecordCompanyOfficerApi = CONTEXT.get("getRecord");
-//
-        String data = fullRecordCompanyOfficerApi.toString();
-        String data2 = CONTEXT.get("deltaData");
+        String json = objectMapper.writeValueAsString(fullRecordCompanyOfficerApi);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -175,9 +139,9 @@ at the start of the data in the string where there shouldn't be one.
         headers.set("ERIC-Identity-Type", "key");
         headers.set("ERIC-Authorised-Key-Privileges", "internal-app");
 
-        HttpEntity<String> request = new HttpEntity<>(data2, headers);
+        HttpEntity<String> request = new HttpEntity<>(json, headers);
         String uri = "/company/" + COMPANY_NUMBER + "/appointments/" + HASHED_APPOINTMENT_ID + "/full_record";
-        String uri2 = "/company/01777777/appointments/EcEKO1YhIKexb0cSDZsn_OHsFw4/full_record";
+
 
         ResponseEntity<Void> response = restTemplate.exchange(uri, HttpMethod.PUT, request, Void.class);
 
@@ -203,28 +167,5 @@ at the start of the data in the string where there shouldn't be one.
     @Then("CHS kafka is invoked successfully")
     public void CHSKafkaInvokedSuccessFully() {
         verify(moreThanOrExactly(1), postRequestedFor(urlEqualTo("/resource-changed")));
-    }
-
-    private FullRecordCompanyOfficerApi buildFullRecordOfficer() {
-        FullRecordCompanyOfficerApi output  = new FullRecordCompanyOfficerApi();
-
-        ExternalData externalData = new ExternalData();
-        Data data = new Data();
-        SensitiveData sensitiveData = new SensitiveData();
-        sensitiveData.setDateOfBirth(new DateOfBirth());
-        externalData.setData(data);
-        externalData.setSensitiveData(sensitiveData);
-        externalData.setAppointmentId("id");
-        externalData.setCompanyNumber("companyNumber");
-        externalData.setInternalId("internalId");
-        externalData.setOfficerId("officerId");
-        externalData.setPreviousOfficerId("previousOfficerId");
-        InternalData internalData = new InternalData();
-        internalData.setOfficerRoleSortOrder(22);
-        internalData.setDeltaAt(CONTEXT.get("appointmentDeltaAt"));
-        internalData.setUpdatedBy("updatedBy");
-        output.setExternalData(externalData);
-        output.setInternalData(internalData);
-        return output;
     }
 }
--- src/it/resources/features/upsert_full_record_appointment.feature
+++ src/it/resources/features/upsert_full_record_appointment.feature
@@ -5,37 +5,42 @@ Feature: Process full record officer information
     Given the company appointments api is running
     And CHS kafka is available
     And the record does not already exist in the database
-    When I send a PUT request with payload "<Data>"
+    When I send a PUT request with payload "<payloadFile>"
     Then CHS kafka is invoked successfully
     And I should receive a 200 status code
     And the record should be saved
 
     Examples:
-      | Data                              | companyNumber | appointmentId               |
-      | natural_officer_full_record_PUT   | 12345678      | 7IjxamNGLlqtIingmTZJJ42Hw9Q |
-      | corporate_officer_full_record_PUT | 01777777      | EcEKO1YhIKexb0cSDZsn_OHsFw4 |
+      | payloadFile                              |
+      | natural_officer_full_record_PUT          |
+      | corporate_officer_full_record_PUT        |
 
   Scenario Outline: Successfully Update a full-record appointment
 
     Given the company appointments api is running
     And CHS kafka is available
-    And the delta for payload "<Data>" is the most recent delta for "<companyNumber>" and "<appointmentId>"
-    When I send a PUT request with payload "<Data>"
+    And the delta for payload "<payloadFile>" is the most recent delta for "<companyNumber>" and "<appointmentId>"
+    When I send a PUT request with payload "<payloadFile>"
     Then CHS kafka is invoked successfully
     And I should receive a 200 status code
     And the record should be saved
 
     Examples:
-    | Data                              | companyNumber | appointmentId               |
-    | natural_officer_full_record_PUT   | 01777777      | 7IjxamNGLlqtIingmTZJJ42Hw9Q |
-    | corporate_officer_full_record_PUT | 01777777      | 7IjxamNGLlqtIingmTZJJ42Hw9Q|
+    | payloadFile                             | companyNumber | appointmentId               |
+    | natural_officer_full_record_PUT         | 01777777      | 7IjxamNGLlqtIingmTZJJ42Hw9Q |
+    | corporate_officer_full_record_PUT       | 01777777      | 7IjxamNGLlqtIingmTZJJ42Hw9Q|
 
   Scenario Outline: Update a full-record appointment - Stale delta
 
-    Given: CHS
+    Given CHS kafka is available
+    And the delta for payload "<payloadFile>" is a stale delta
+    When I send a PUT request with payload "<payloadFile>"
+    Then a request should NOT be sent to the resource changed endpoint
+    And the changes within the delta should NOT be persisted in the database.
 
 
     Examples:
-      | Data                              |
-      | natural_officer_full_record_PUT   |
-      | corporate_officer_full_record_PUT |
+      | payloadFile                        |
+      | natural_officer_full_record_PUT    |
+      | corporate_officer_full_record_PUT  |
+
