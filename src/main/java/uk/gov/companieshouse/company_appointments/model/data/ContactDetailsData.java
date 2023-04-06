b418991dfa698b232159bc81b433f4fc1c9c6df6
kinpang-CH <kpang@companieshouse.gov.uk>
2023-04-06 08:48:41 +0100

    Revert unrelated file back to master vesion

--- src/main/java/uk/gov/companieshouse/company_appointments/model/data/ContactDetailsData.java
+++ src/main/java/uk/gov/companieshouse/company_appointments/model/data/ContactDetailsData.java
@@ -1,6 +1,7 @@
 package uk.gov.companieshouse.company_appointments.model.data;
 
 import java.util.Objects;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 public class ContactDetailsData {
 
