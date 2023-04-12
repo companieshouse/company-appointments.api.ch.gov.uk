e9e82810a12c35c55d6d9ee912b13406b5dde97a
cgregoryCH <cgregory@companieshouse.gov.uk>
2023-04-12 15:57:06 +0100

    Add integration test to ensure fields are mapped to snake case.

--- src/it/java/uk/gov/companieshouse/company_appointments/tests/CompanyAppointmentFullRecordControllerITest.java
+++ src/it/java/uk/gov/companieshouse/company_appointments/tests/CompanyAppointmentFullRecordControllerITest.java
@@ -14,6 +14,7 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -27,7 +28,12 @@ import uk.gov.companieshouse.company_appointments.model.data.CompanyAppointmentD
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Testcontainers
@@ -72,6 +78,34 @@ class CompanyAppointmentFullRecordControllerITest {
         query.addCriteria(Criteria.where("_id").is("7IjxamNGLlqtIingmTZJJ42Hw9Q"));
         List<CompanyAppointmentDocument> appointments = mongoTemplate.find(query, CompanyAppointmentDocument.class);
         assertThat(appointments, is(empty()));
+    }
+
+    @Test
+    void testReturnFullRecordGetMapsFieldNamesCorrectly() throws Exception{
+        mockMvc.perform(get("/company/{company_number}/appointments/{appointment_id}/full_record", COMPANY_NUMBER, OFFICER_ID)
+                        .header("ERIC-Identity", "123")
+                        .header("ERIC-Identity-Type", "key")
+                        .header("ERIC-authorised-key-privileges", "sensitive-data"))
+                .andExpect(jsonPath("$.service_address.address_line_1", notNullValue()))
+                .andExpect(jsonPath("$.service_address.address_line_2", notNullValue()))
+                .andExpect(jsonPath("$.service_address.postal_code", notNullValue()))
+                .andExpect(jsonPath("$.usual_residential_address.address_line_1", notNullValue()))
+                .andExpect(jsonPath("$.usual_residential_address.address_line_2", notNullValue()))
+                .andExpect(jsonPath("$.usual_residential_address.care_of", notNullValue()))
+                .andExpect(jsonPath("$.usual_residential_address.po_box", notNullValue()))
+                .andExpect(jsonPath("$.usual_residential_address.postal_code", notNullValue()))
+                .andExpect(jsonPath("$.identification.identification_type", notNullValue()))
+                .andExpect(jsonPath("$.identification.legal_authority", notNullValue()))
+                .andExpect(jsonPath("$.identification.legal_form", notNullValue()))
+                .andExpect(jsonPath("$.identification.place_registered", notNullValue()))
+                .andExpect(jsonPath("$.identification.registration_number", notNullValue()))
+                .andExpect(jsonPath("$.contact_details.contact_name", notNullValue()))
+                .andExpect(jsonPath("$.principal_office_address.address_line_1", notNullValue()))
+                .andExpect(jsonPath("$.principal_office_address.address_line_2", notNullValue()))
+                .andExpect(jsonPath("$.principal_office_address.care_of", notNullValue()))
+                .andExpect(jsonPath("$.principal_office_address.po_box", notNullValue()))
+                .andExpect(jsonPath("$.principal_office_address.postal_code", notNullValue()))
+                .andExpect(status().isOk());
     }
 
     @Test
