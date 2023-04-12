2ff9378a955f31949067646e84aa694f009b1768
crowlandsCH <crowlands@companieshouse.gov.uk>
2023-04-12 17:44:06 +0100

    DSND-1536: Add new integration test for FullRecordController to improve coverage

--- src/it/java/uk/gov/companieshouse/company_appointments/tests/CompanyAppointmentFullRecordControllerITest.java
+++ src/it/java/uk/gov/companieshouse/company_appointments/tests/CompanyAppointmentFullRecordControllerITest.java
@@ -2,62 +2,54 @@ package uk.gov.companieshouse.company_appointments.tests;
 
 import org.apache.commons.io.IOUtils;
 import org.bson.Document;
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import uk.gov.companieshouse.company_appointments.exception.NotFoundException;
-import uk.gov.companieshouse.company_appointments.exception.ServiceUnavailableException;
-import uk.gov.companieshouse.company_appointments.service.CompanyAppointmentFullRecordService;
+import uk.gov.companieshouse.company_appointments.CompanyAppointmentsApplication;
+import uk.gov.companieshouse.company_appointments.api.ResourceChangedApiService;
+import uk.gov.companieshouse.company_appointments.model.data.CompanyAppointmentDocument;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Collections;
+import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Testcontainers
 @AutoConfigureMockMvc
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = CompanyAppointmentsApplication.class, webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CompanyAppointmentFullRecordControllerITest {
 
     private static final String COMPANY_NUMBER = "12345678";
     private static final String APPOINTMENT_ID = "7IjxamNGLlqtIingmTZJJ42Hw9Q";
-    private static final String CONTEXT_ID = "5234234234";
-    private static final String APPOINTMENT_URL = "/company/%s/appointments/%s/full_record/delete";
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @MockBean
-    private CompanyAppointmentFullRecordService companyAppointmentService;
 
     @Autowired
     private MockMvc mockMvc;
 
     @Container
     private static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4");
+
+    @MockBean
+    private ResourceChangedApiService resourceChangedApiService;
 
     private static MongoTemplate mongoTemplate;
 
@@ -73,22 +65,20 @@ class CompanyAppointmentFullRecordControllerITest {
     }
 
     @Test
-    @DisplayName("DELETE appointment full record")
-    void testReturn200OkToDeleteAppointment() {
+    void testReturn200IfOfficerIsDeleted() throws Exception{
+        ResultActions result = mockMvc
+                .perform(delete("/company/{company_number}/appointments/{appointment_id}/full_record/delete", COMPANY_NUMBER,
+                        APPOINTMENT_ID)
+                        .header("ERIC-Identity", "123").header("ERIC-Identity-Type", "key")
+                        .header("ERIC-authorised-key-privileges", "internal-app")
+                        .header("x-request-id", "contextId"));
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.set("x-request-id", "5234234234");
-        headers.add("ERIC-Identity" , "SOME_IDENTITY");
-        headers.add("ERIC-Identity-Type", "key");
-        headers.add("ERIC-Authorised-Key-Privileges", "internal-app");
+        result.andExpect(status().isOk());
 
-        HttpEntity<String> request = new HttpEntity<>(null, headers);
-        ResponseEntity<Void> responseEntity = restTemplate.exchange(String.format(APPOINTMENT_URL, COMPANY_NUMBER, APPOINTMENT_ID),
-                HttpMethod.DELETE, request, Void.class);
-
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Query query = new Query();
+        query.addCriteria(Criteria.where("_id").is("7IjxamNGLlqtIingmTZJJ42Hw9Q"));
+        List<CompanyAppointmentDocument> appointments = mongoTemplate.find(query, CompanyAppointmentDocument.class);
+        MatcherAssert.assertThat(appointments, is(empty()));
     }
 
     @Test
@@ -120,24 +110,19 @@ class CompanyAppointmentFullRecordControllerITest {
     }
 
     @Test
-    @DisplayName("Return not found if appointment id incorrect")
-    void testReturn404NotFoundIfAppointmentNotDeleted() throws NotFoundException, ServiceUnavailableException {
+    void testReturn404IfOfficerIsNotDeleted() throws Exception{
+        ResultActions result = mockMvc
+                .perform(delete("/company/{company_number}/appointments/{appointment_id}/full_record/delete", COMPANY_NUMBER,
+                        "Incorrect")
+                        .header("ERIC-Identity", "123").header("ERIC-Identity-Type", "key")
+                        .header("ERIC-authorised-key-privileges", "internal-app")
+                        .header("x-request-id", "contextId"));
 
-        doThrow(new NotFoundException(String.format("Appointment [%s] for company [%s] not found", "incorrect", COMPANY_NUMBER)))
-                .when(companyAppointmentService).deleteAppointmentDelta(CONTEXT_ID, COMPANY_NUMBER, "incorrect");
+        result.andExpect(status().isNotFound());
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.set("x-request-id", "5234234234");
-        headers.add("ERIC-Identity" , "SOME_IDENTITY");
-        headers.add("ERIC-Identity-Type", "key");
-        headers.add("ERIC-Authorised-Key-Privileges", "internal-app");
-
-        HttpEntity<String> request = new HttpEntity<>(null, headers);
-        ResponseEntity<Void> responseEntity = restTemplate.exchange(String.format(APPOINTMENT_URL, COMPANY_NUMBER, "incorrect"),
-                HttpMethod.DELETE, request, Void.class);
-
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        Query query = new Query();
+        query.addCriteria(Criteria.where("_id").is("7IjxamNGLlqtIingmTZJJ42Hw9Q"));
+        List<CompanyAppointmentDocument> appointments = mongoTemplate.find(query, CompanyAppointmentDocument.class);
+        MatcherAssert.assertThat(appointments.size(), is(1));
     }
 }
