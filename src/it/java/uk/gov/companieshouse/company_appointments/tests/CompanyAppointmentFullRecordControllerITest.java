0d056cbcb30c82473c575e1adccbf22aaa69017a
sthompsonCH <sthompson@companieshouse.gov.uk>
2023-07-13 14:43:18 +0100

    DSND-1823: Remove unused import

--- src/it/java/uk/gov/companieshouse/company_appointments/tests/CompanyAppointmentFullRecordControllerITest.java
+++ src/it/java/uk/gov/companieshouse/company_appointments/tests/CompanyAppointmentFullRecordControllerITest.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
 import org.bson.Document;
 import org.junit.jupiter.api.BeforeAll;
