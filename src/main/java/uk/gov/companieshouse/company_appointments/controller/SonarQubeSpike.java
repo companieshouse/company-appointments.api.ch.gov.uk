5f66ec5ee726df4561be31694dbcfbe1bddb673d
sthompsonCH <sthompson@companieshouse.gov.uk>
2023-11-20 12:38:31 +0000

    DSND-1990: SonarQube Spike Test
    * Create a java class to test the sonar qube coverage on github.
    * The resulting PR will ultimately be deleted.

---
+++ src/main/java/uk/gov/companieshouse/company_appointments/controller/SonarQubeSpike.java
@@ -0,0 +1,12 @@
+package uk.gov.companieshouse.company_appointments.controller;
+
+public class SonarQubeSpike {
+
+    public SonarQubeSpike() {
+
+    }
+
+    public int calculate(int first, int second) {
+        return first + second;
+    }
+}
