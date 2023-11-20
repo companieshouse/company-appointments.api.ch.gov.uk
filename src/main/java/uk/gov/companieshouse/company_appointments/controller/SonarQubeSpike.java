cb3fd5c46448ca7e06ebe27d12e5ccc0f7aaec94
sthompsonCH <sthompson@companieshouse.gov.uk>
2023-11-20 13:44:34 +0000

    Remove project key
    * Project key was assigned to company-metrics-consumer, not
      appointments api due to copying tag from metrics consumer.

--- src/main/java/uk/gov/companieshouse/company_appointments/controller/SonarQubeSpike.java
+++ src/main/java/uk/gov/companieshouse/company_appointments/controller/SonarQubeSpike.java
@@ -3,7 +3,7 @@ package uk.gov.companieshouse.company_appointments.controller;
 public class SonarQubeSpike {
 
     public int calculate(int first, int second) {
-        final int result = first + second;
+        int result = first + second;
         return result;
     }
 }
