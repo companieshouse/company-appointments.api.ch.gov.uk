bc78021d5a6ab26cabba4428dd90d496876b059a
sthompsonCH <sthompson@companieshouse.gov.uk>
2023-11-20 12:46:40 +0000

    DSND-1990: Add itest

---
+++ src/it/java/uk/gov/companieshouse/company_appointments/tests/SonarQubeSpikeITest.java
@@ -0,0 +1,21 @@
+package uk.gov.companieshouse.company_appointments.tests;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.companieshouse.company_appointments.controller.SonarQubeSpike;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SonarQubeSpikeITest {
+
+    @Test
+    void test() {
+        // given
+        SonarQubeSpike sonarQubeSpike = new SonarQubeSpike();
+
+        // when
+        int result = sonarQubeSpike.calculate(1, 1);
+
+        // then
+        assertEquals(2, result);
+    }
+}
