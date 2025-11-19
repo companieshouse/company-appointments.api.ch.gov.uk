6315ff1dcdb834fa7c2d5fcb77f524949b559b08
sunilmurugesan <smurugesan@companieshouse.gov.uk>
2025-11-19 09:08:52 +0000

    ASM-738 - Fixes for Sonar violations.

--- src/main/java/uk/gov/companieshouse/company_appointments/officerappointments/FormerNamesMapper.java
+++ src/main/java/uk/gov/companieshouse/company_appointments/officerappointments/FormerNamesMapper.java
@@ -3,7 +3,6 @@ package uk.gov.companieshouse.company_appointments.officerappointments;
 import static java.util.Optional.ofNullable;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 import uk.gov.companieshouse.api.officer.FormerNames;
 import uk.gov.companieshouse.company_appointments.model.data.DeltaFormerNames;
